7db94dae815d017ab5463b5baef38085b512d86d
horoshi10v <valentyn.khoroshylov@gmail.com>
2021-12-15 00:23:39 +0200

    web Design homework

--- main.go
+++ main.go
@@ -1,7 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func distance(x, y, x2, y2 float64) float64 {
+	a := x2 - x
+	b := y2 - y
+	return math.Sqrt(a*a + b*b)
+}
+
+func (r *Rectangle) Area() float64 {
+	l := distance(r.x, r.y, r.x, r.y2)
+	w := distance(r.x, r.y, r.x2, r.y)
+	return l * w
+}
+
+func (c *Circle) Area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+type Position struct {
+	x, y float64
+}
+
+type Rectangle struct {
+	Position //x and y
+	x2, y2   float64
+}
+type Circle struct {
+	Position
+	r float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func totalArea(shape ...Shape) (total float64) {
+	for _, shape := range shape {
+		total += shape.Area()
+	}
+	return total
+}
 
 func main() {
-	fmt.Printf("Hello NIX!")
+
+	rectangle := &Rectangle{
+		Position: Position{
+			x: 0,
+			y: 0,
+		},
+		x2: 10,
+		y2: 10,
+	}
+
+	circle := &Circle{
+		r: 5,
+	}
+	circle2 := &Circle{
+		r: 5,
+	}
+
+	fmt.Println(rectangle.Area())
+	fmt.Println(circle.Area())
+	fmt.Println(totalArea(circle, rectangle, circle2, circle2))
 }
